937f32a03dc4c4e412725694f86526fe1c5860d3
whospeo <peolo.dev@gmail.com>
2025-11-23 18:03:05 +0100

    Initial commit

---
+++ src/main/java/com/whospeo/magnetium/MagnetiumClient.java
@@ -0,0 +1,10 @@
+package com.whospeo.magnetium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagnetiumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/whospeo/magnetium/MagnetiumDataGenerator.java
@@ -0,0 +1,11 @@
+package com.whospeo.magnetium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MagnetiumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
