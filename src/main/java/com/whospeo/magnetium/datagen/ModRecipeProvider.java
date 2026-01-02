164719afd9ad1dfbd794fe03ca4c9d60b6e9e363
whospeo <peolo.dev@gmail.com>
2026-01-03 00:14:29 +0100

    Updated to 1.21.3

--- src/main/java/com/whospeo/magnetium/datagen/ModRecipeProvider.java
+++ src/main/java/com/whospeo/magnetium/datagen/ModRecipeProvider.java
@@ -4,8 +4,7 @@ import com.whospeo.magnetium.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.RecipeGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
@@ -18,67 +17,77 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(RecipeExporter recipeExporter) {
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup wrapperLookup, RecipeExporter recipeExporter) {
+        return new RecipeGenerator(wrapperLookup, recipeExporter) {
+            @Override
+            public void generate() {
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MAGNET)
-                .pattern("N S")
-                .pattern("III")
-                .pattern("   ")
-                .input('N', ModItems.NORTH_POLE)
-                .input('S', ModItems.SOUTH_POLE)
-                .input('I', Items.IRON_INGOT)
-                .criterion(hasItem(ModItems.NORTH_POLE), conditionsFromItem(ModItems.NORTH_POLE))
-                .criterion(hasItem(ModItems.SOUTH_POLE), conditionsFromItem(ModItems.SOUTH_POLE))
-                .offerTo(recipeExporter);
+                createShaped(RecipeCategory.MISC, ModItems.MAGNET)
+                        .pattern("N S")
+                        .pattern("III")
+                        .pattern("   ")
+                        .input('N', ModItems.NORTH_POLE)
+                        .input('S', ModItems.SOUTH_POLE)
+                        .input('I', Items.IRON_INGOT)
+                        .criterion(hasItem(ModItems.NORTH_POLE), conditionsFromItem(ModItems.NORTH_POLE))
+                        .criterion(hasItem(ModItems.SOUTH_POLE), conditionsFromItem(ModItems.SOUTH_POLE))
+                        .offerTo(recipeExporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MAGNET_TIER_2)
-                .pattern("GGG")
-                .pattern("GMG")
-                .pattern("GGG")
-                .input('G', Items.GOLD_INGOT)
-                .input('M', ModItems.MAGNET)
-                .criterion(hasItem(ModItems.MAGNET), conditionsFromItem(ModItems.MAGNET))
-                .offerTo(recipeExporter);
+                createShaped(RecipeCategory.MISC, ModItems.MAGNET_TIER_2)
+                        .pattern("GGG")
+                        .pattern("GMG")
+                        .pattern("GGG")
+                        .input('G', Items.GOLD_INGOT)
+                        .input('M', ModItems.MAGNET)
+                        .criterion(hasItem(ModItems.MAGNET), conditionsFromItem(ModItems.MAGNET))
+                        .offerTo(recipeExporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MAGNET_TIER_3)
-                .pattern("DDD")
-                .pattern("DMD")
-                .pattern("DDD")
-                .input('D', Items.DIAMOND)
-                .input('M', ModItems.MAGNET_TIER_2)
-                .criterion(hasItem(ModItems.MAGNET_TIER_2), conditionsFromItem(ModItems.MAGNET_TIER_2))
-                .offerTo(recipeExporter);
+                createShaped(RecipeCategory.MISC, ModItems.MAGNET_TIER_3)
+                        .pattern("DDD")
+                        .pattern("DMD")
+                        .pattern("DDD")
+                        .input('D', Items.DIAMOND)
+                        .input('M', ModItems.MAGNET_TIER_2)
+                        .criterion(hasItem(ModItems.MAGNET_TIER_2), conditionsFromItem(ModItems.MAGNET_TIER_2))
+                        .offerTo(recipeExporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MAGNETIC_HAMMER)
-                .pattern("NIS")
-                .pattern(" I ")
-                .pattern(" I ")
-                .input('N', ModItems.NORTH_POLE)
-                .input('S', ModItems.SOUTH_POLE)
-                .input('I', ModItems.IRON_STICK)
-                .criterion(hasItem(ModItems.NORTH_POLE), conditionsFromItem(ModItems.NORTH_POLE))
-                .criterion(hasItem(ModItems.SOUTH_POLE), conditionsFromItem(ModItems.SOUTH_POLE))
-                .offerTo(recipeExporter);
+                createShaped(RecipeCategory.MISC, ModItems.MAGNETIC_HAMMER)
+                        .pattern("NIS")
+                        .pattern(" I ")
+                        .pattern(" I ")
+                        .input('N', ModItems.NORTH_POLE)
+                        .input('S', ModItems.SOUTH_POLE)
+                        .input('I', ModItems.IRON_STICK)
+                        .criterion(hasItem(ModItems.NORTH_POLE), conditionsFromItem(ModItems.NORTH_POLE))
+                        .criterion(hasItem(ModItems.SOUTH_POLE), conditionsFromItem(ModItems.SOUTH_POLE))
+                        .offerTo(recipeExporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.IRON_STICK)
-                .pattern(" I ")
-                .pattern(" I ")
-                .pattern("   ")
-                .input('I', Items.IRON_INGOT)
-                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
-                .offerTo(recipeExporter);
+                createShaped(RecipeCategory.MISC, ModItems.IRON_STICK)
+                        .pattern(" I ")
+                        .pattern(" I ")
+                        .pattern("   ")
+                        .input('I', Items.IRON_INGOT)
+                        .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                        .offerTo(recipeExporter);
 
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.NORTH_POLE)
-                .input(Items.RED_DYE)
-                .input(Items.IRON_INGOT)
-                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
-                .offerTo(recipeExporter);
+                createShapeless(RecipeCategory.MISC, ModItems.NORTH_POLE)
+                        .input(Items.RED_DYE)
+                        .input(Items.IRON_INGOT)
+                        .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                        .offerTo(recipeExporter);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.SOUTH_POLE)
-                .input(Items.BLUE_DYE)
-                .input(Items.IRON_INGOT)
-                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
-                .offerTo(recipeExporter);
+                createShapeless(RecipeCategory.MISC, ModItems.SOUTH_POLE)
+                        .input(Items.BLUE_DYE)
+                        .input(Items.IRON_INGOT)
+                        .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                        .offerTo(recipeExporter);
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "Magnetium Recipes";
     }
 }
--- src/main/java/com/whospeo/magnetium/item/ModItems.java
+++ src/main/java/com/whospeo/magnetium/item/ModItems.java
@@ -8,31 +8,34 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.function.Function;
+
 public class ModItems {
 
     public static final Item NORTH_POLE = registerItem("north_pole",
-            new Item(new Item.Settings().rarity(Rarity.RARE)));
+            setting -> new Item(setting.rarity(Rarity.RARE)));
     public static final Item SOUTH_POLE = registerItem("south_pole",
-            new Item(new Item.Settings().rarity(Rarity.RARE)));
+            setting -> new Item(setting.rarity(Rarity.RARE)));
     public static final Item IRON_STICK = registerItem("iron_stick",
-            new Item(new Item.Settings().rarity(Rarity.COMMON)));
+            setting -> new Item(setting.rarity(Rarity.COMMON)));
 
     public static final Item MAGNET = registerItem("magnet_tier_1",
-            new Item(new Item.Settings().maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
+            setting -> new Item(setting.maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
     public static final Item MAGNET_TIER_2 = registerItem("magnet_tier_2",
-            new Item(new Item.Settings().maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
+            setting -> new Item(setting.maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
     public static final Item MAGNET_TIER_3 = registerItem("magnet_tier_3",
-            new Item(new Item.Settings().maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
+            setting -> new Item(setting.maxCount(1).maxDamage(0).rarity(Rarity.EPIC)));
 
     public static final Item MAGNETIC_HAMMER = registerItem("magnetic_hammer",
-            new HammerItem(ToolMaterials.NETHERITE, new Item.Settings()
-                    .attributeModifiers(PickaxeItem.createAttributeModifiers(ToolMaterials.NETHERITE, 1, -2.8f))));
+            setting -> new HammerItem(ToolMaterial.NETHERITE, 1, -2.8f, setting));
 
     public static final double COLLECTION_RADIUS_TIER_1 = 9.0;
     public static final double COLLECTION_RADIUS_TIER_2 = 12.0;
@@ -105,17 +108,13 @@ public class ModItems {
         }
     }
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Magnetium.MOD_ID, name), item);
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(Magnetium.MOD_ID, name),
+        function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Magnetium.MOD_ID, name)))));
     }
 
     public static void registerModItems() {
         Magnetium.LOGGER.info("Registering Mod Items for " + Magnetium.MOD_ID);
-
-        /*ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(NORTH_POLE);
-            entries.add(SOUTH_POLE);
-        });*/
 
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             if (server.getTicks() % COLLECTION_COOLDOWN == 0) {
