df287ed5746c5b9c1bbe1184e02b15f43f8a5a3d
whospeo <peolo.dev@gmail.com>
2026-01-03 00:32:38 +0100

    Updated to 1.21.4

--- src/main/java/com/whospeo/magnetium/datagen/ModModelProvider.java
+++ src/main/java/com/whospeo/magnetium/datagen/ModModelProvider.java
@@ -1,11 +1,11 @@
 package com.whospeo.magnetium.datagen;
 
 import com.whospeo.magnetium.item.ModItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
+import net.minecraft.client.data.Models;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
